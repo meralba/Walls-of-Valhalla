480955c2634a3941e9e69bc0652099dfa86d8010
Mer Alba <malbamoyano@gmail.com>
2016-04-30 17:10:33 +0200

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MovingEntity {
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+	}
+
+
+    protected override void OnCantMove<T>(T component)
+    {
+
+    }
+
+    public void MoveEnemy()
+    {
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    // Hacemos la clase un singleton
+    public static GameManager instance = null;
+
+    public float turnDelay = 0.1f;
+    public float levelStartDelay = 2f;
+
+    private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+
+    public int playerHP = 100;
+    [HideInInspector]
+    public bool playersTurn = true;
+
+    private int level = 1;                                  //Current level number, expressed in game as "Day 1".
+    private GameObject levelImage;
+    private Text levelText;
+    // Lista de enemigos
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
+    private bool doingSetup;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        //Get a component reference to the attached BoardManager script
+        boardScript = GetComponent<BoardManager>();
+
+        this.enemies = new List<Enemy>();
+
+        //Call the InitGame function to initialize the first level 
+        InitGame();
+    }
+
+    private void OnLevelWasLoaded(int index)
+    {
+        level++;
+
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        doingSetup = true;
+
+        levelImage = GameObject.Find("LevelImage");
+        levelText = GameObject.Find("LevelText").GetComponent<Text>();
+        levelText.text = "Day " + level;
+        levelImage.SetActive(true);
+        Invoke("HideLevelImage", levelStartDelay);
+
+        enemies.Clear();
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
+        boardScript.SetupScene(level);
+
+
+    }
+
+    private void HideLevelImage()
+    {
+        levelImage.SetActive(false);
+
+        doingSetup = false;
+    }
+
+    public void GameOver()
+    {
+        levelText.text = "After " + level + " days, you starved.";
+        levelImage.SetActive(true);
+        enabled = false;
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+        if (playersTurn || enemiesMoving || doingSetup)
+            return;
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(this.turnDelay);
+
+        if (enemies.Count == 0)
+            yield return new WaitForSeconds(this.turnDelay);
+
+        for (int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(this.turnDelay);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
+    }
+}
